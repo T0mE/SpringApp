4cc4cccdf46ff69816a2fde7b4f485fb6542d3c7
Tomek <qomekp@gmail.com>
2020-12-28 19:50:14 +0100

    Clean project

---
+++ src/main/java/tom/springFramework/spring5appweb/Spring5appwebApplication.java
@@ -0,0 +1,13 @@
+package tom.springFramework.spring5appweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5appwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5appwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tom/springFramework/spring5appweb/Spring5appwebApplicationTests.java
@@ -0,0 +1,13 @@
+package tom.springFramework.spring5appweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5appwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
